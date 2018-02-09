05c2b6ea65784f245349c36554ec645ec9aa681f
Henry Chibolyo <heinrighdisobayish.hd@gmail.com>
2018-02-09 12:45:30 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="title"><h1>SIGN IN</h1></div>
+    <div class="container">
+      <div class="left"></div>
+      <div class="right">
+        <div class="formBox">
+          <form >
+            <p>User Name</p>
+            <input type="text" name="" placeholder="name">
+            <p>Password</p>
+            <input type="password" name="" placeholder="password"><br /><br />
+            <input type="submit" value="SIGN IN" name="">
+            <a href="#"> SIGN UP</a>
+          </form
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
